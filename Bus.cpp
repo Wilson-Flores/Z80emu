ebec9c574cefff8d5aff8069f93f1fbac08498f6
Wilson Flores <WillyFlores173@gmail.com>
2024-01-27 14:05:25 -0500

    added register reset to bus reset function.

--- Bus.cpp
+++ Bus.cpp
@@ -29,6 +29,37 @@ void Bus::reset() {
 		i = 0x00;
 	}
 	std::cout << "Memmory Cleared!\n";
-	cpu.program_counter = 0x00;
+	cpu.accumulator = 0x00;
+	cpu.B_register = 0x00;
+	cpu.C_register = 0x00;
+	cpu.D_register = 0x00;
+	cpu.E_register = 0x00;
+	cpu.H_register = 0x00;
+	cpu.L_register = 0x00;
+	cpu.flag_register = 0x00;
 
+
+	// Alternate Registers
+	cpu.alt_accumulator = 0x00;
+	cpu.alt_B_register = 0x00;
+	cpu.alt_C_register = 0x00;
+	cpu.alt_D_register = 0x00;
+	cpu.alt_E_register = 0x00;
+	cpu.alt_H_register = 0x00;
+	cpu.alt_L_register = 0x00;
+	cpu.alt_flag_register = 0x00;
+
+	// Index Registers
+	cpu.index_register_x = 0x0000;
+	cpu.index_register_y = 0x0000;
+
+	// Other Registers
+	cpu.interrupt_vector_register = 0x00;
+	cpu.memory_refresh_register = 0x00;
+	cpu.stack_pointer = 0x0000;
+	cpu.program_counter = 0x0000;
+
+	// Interrupts
+	cpu.interrupt_enable_flip_flop_1 = false;
+	cpu.interrupt_enable_flip_flop_2 = false;
 }
