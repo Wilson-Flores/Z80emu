0a7b2f68f84c5ce5939d8284a6cb4e28783d744f
Wilson-Flores <WillyFlores173@gmail.com>
2024-04-28 21:54:22 -0400

    fixed 16bit ADC function.

--- instructions/Add.cpp
+++ instructions/Add.cpp
@@ -338,13 +338,13 @@ void z80cpu::ADC_implied_register_extended() {
     uint8_t register_pair_bit = (opcode & BIT_MASK_3) >> 4;
     uint8_t high_byte = *register_pair_table_ss[register_pair_bit].high_byte_register;
     uint8_t low_byte = *register_pair_table_ss[register_pair_bit].low_byte_register;
-    uint16_t register_pair_data = ( high_byte << 8) + low_byte;
+    uint16_t register_pair_data = (high_byte << 8) + low_byte;
     uint16_t register_pair_data_with_carry = register_pair_data + static_cast<uint16_t>(get_flag(CARRY_FLAG));
     uint16_t HL_data = (H_register << 8) + L_register;
     uint16_t result = HL_data + register_pair_data_with_carry;
 
     // S is set if result is negative, else reset
-    set_flag(CARRY_FLAG, result & 0x8000);
+    set_flag(SIGN_FLAG, result & 0x8000);
     // Z is set if result is 0, else reset
     set_flag(ZERO_FLAG, result == 0x0000);
     // H is set if carry from bit 11, else reset
@@ -355,7 +355,7 @@ void z80cpu::ADC_implied_register_extended() {
     // N is reset
     set_flag(ADD_SUB_FLAG, false);
     // C is set if carry from bit 15, else reset
-    set_flag(CARRY_FLAG, (result < HL_data) || (result < register_pair_data));
+    set_flag(CARRY_FLAG, (static_cast<uint32_t>(result) > 0xFFFF));
 
     H_register = (result & 0xFF00) >> 8;
     L_register = result & 0x00FF;
