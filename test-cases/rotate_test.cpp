07d85bb1de34cc7b0c4011cf5449ba4bb03fc758
Wilson-Flores <WillyFlores173@gmail.com>
2024-06-03 21:26:44 -0400

    added RLC test function.

--- test-cases/rotate_test.cpp
+++ test-cases/rotate_test.cpp
@@ -1,3 +1,4 @@
+#include <unordered_map>
 #include "rotate_test.hpp"
 
 
@@ -11,6 +12,7 @@ void rotate_test_choice(Bus& test){
             RLCA_test(test);
             break;
         case 2:
+            RLC_test(test);
             break;
         case 3:
             break;
@@ -45,5 +47,32 @@ void RLCA_test(Bus& test){
 
     for(uint16_t byte_counter = 0; byte_counter < 4; byte_counter++){
         test.cpu.instruction_cycle();
+    }
+}
+
+
+void RLC_test(Bus& test){
+    std::unordered_map<uint8_t, uint8_t> registers =
+            {{0x3E, 0x07}, // A
+             {0x06, 0x00}, // B
+             {0x0E, 0x01}, // C
+             {0x16, 0x02}, // D
+             {0x1E, 0x03}, // E
+             {0x26, 0x04}, // H
+             {0x2E, 0x05}  // L
+             };
+
+    for (const std::pair<const uint8_t, uint8_t>& pair : registers) {
+        std::vector<uint8_t> memory = {pair.first, 0x88, 0xCB, pair.second, pair.first, 0x6B, 0xCB, pair.second};
+
+        test.rom_reset();
+
+        for (int i = 0; i < memory.size(); i++) {
+            test.rom_write(i,memory[i]);
+        }
+
+        for(uint16_t byte_counter = 0; byte_counter < 4; byte_counter++){
+            test.cpu.instruction_cycle();
+        }
     }
 }
--- test-cases/rotate_test.hpp
+++ test-cases/rotate_test.hpp
@@ -4,12 +4,15 @@
 #include "../Bus.hpp"
 #include <iomanip>
 #include <iostream>
+#include <unordered_map>
 
 
 
 void rotate_test_choice(Bus& test);
 
 void RLCA_test(Bus& test);
+
+void RLC_test(Bus& test);
 
 
 #endif //Z80EMU_ROTATE_TEST_HPP
