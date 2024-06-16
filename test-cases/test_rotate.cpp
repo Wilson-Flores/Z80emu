92ed79f687170975473279db578807ca20078cbc
Wilson Flores <WillyFlores173@gmail.com>
2024-06-16 08:20:40 -0400

    Added test cases for RL instructions.

--- test-cases/test_rotate.cpp
+++ test-cases/test_rotate.cpp
@@ -322,3 +322,161 @@ TEST_F(RotateTest, RRC_indexed_iy_test){
         ASSERT_EQ(compare_flag_values, false);
     }
 }
+
+
+TEST_F(RotateTest, RLA_implied_test){
+    std::vector<uint8_t> expected_flag_values = {0x00, 0x01, 0x00, 0x00, 0x01};
+    bool compare_flag_values = false;
+
+    std::vector<uint8_t> memory = {0x3E, 0x76, 0x3F, 0x17, 0x3E, 0x8F, 0x17};
+
+    for (int i = 0; i < memory.size(); i++) {
+        bus.rom_write(i,memory[i]);
+    }
+
+    for(uint16_t byte_counter = 0; byte_counter < expected_flag_values.size(); byte_counter++){
+        bus.cpu.instruction_cycle();
+
+        if(bus.cpu.flag_register != expected_flag_values[byte_counter]){
+            compare_flag_values = true;
+            std::cout << "OPCODE: " << std::setfill('0') << std::setw(2) << std::hex << std::uppercase
+                      << static_cast<int>(bus.cpu.opcode) << '\t';
+
+            std::cout << "Flag: 0x" << std::setfill('0') << std::setw(2) << std::hex << std::uppercase
+                      << static_cast<int>(bus.cpu.flag_register) << '\n';
+        }
+        ASSERT_EQ(compare_flag_values, false);
+    }
+
+}
+
+
+TEST_F(RotateTest, RL_implied_test){
+    // {first, second}
+    // {[LD r, n], [RL r]}
+    std::vector<std::pair<uint8_t, uint8_t>> registers_instructions =
+            {{0x3E, 0x17}, // A
+             {0x06, 0x10}, // B
+             {0x0E, 0x11}, // C
+             {0x16, 0x12}, // D
+             {0x1E, 0x13}, // E
+             {0x26, 0x14}, // H
+             {0x2E, 0x15}  // L
+            };
+
+    std::vector<uint8_t> expected_flag_values = { 0x00, 0x01, 0x84, 0x84, 0x05};
+    bool compare_flag_values = false;
+
+    for (const std::pair<const uint8_t, uint8_t>& pair : registers_instructions) {
+        std::vector<uint8_t> memory = {pair.first, 0x76, 0x3F, 0xCB, pair.second, pair.first, 0x8F, 0xCB, pair.second};
+
+        bus.rom_reset();
+
+        for (int i = 0; i < memory.size(); i++) {
+            bus.rom_write(i,memory[i]);
+        }
+
+        for(uint16_t byte_counter = 0; byte_counter < expected_flag_values.size(); byte_counter++){
+            bus.cpu.instruction_cycle();
+
+            if(bus.cpu.flag_register != expected_flag_values[byte_counter]){
+                compare_flag_values = true;
+                std::cout << "OPCODE: " << std::setfill('0') << std::setw(2) << std::hex << std::uppercase
+                          << static_cast<int>(bus.cpu.opcode) << '\t';
+
+                std::cout << "Flag: 0x" << std::setfill('0') << std::setw(2) << std::hex << std::uppercase
+                          << static_cast<int>(bus.cpu.flag_register) << '\n';
+            }
+            ASSERT_EQ(compare_flag_values, false);
+        }
+    }
+}
+
+
+TEST_F(RotateTest, RL_indirect_test){
+    std::vector<uint8_t> expected_flag_values = {0x00, 0x00, 0x01, 0x84, 0x84, 0x05};
+    bool compare_flag_values = false;
+
+    std::vector<uint8_t> memory = {0x01 ,0x0F, 0x00, 0x36, 0x76, 0x3F, 0xCB, 0x16, 0x36, 0x8F, 0xCB, 0x16};
+
+    for (int i = 0; i < memory.size(); i++) {
+        bus.rom_write(i,memory[i]);
+    }
+
+    for(uint16_t byte_counter = 0; byte_counter < expected_flag_values.size(); byte_counter++){
+        bus.cpu.instruction_cycle();
+
+        if(bus.cpu.flag_register != expected_flag_values[byte_counter]){
+            compare_flag_values = true;
+            std::cout << "OPCODE: " << std::setfill('0') << std::setw(2) << std::hex << std::uppercase
+                      << static_cast<int>(bus.cpu.opcode) << '\t';
+
+            std::cout << "Flag: 0x" << std::setfill('0') << std::setw(2) << std::hex << std::uppercase
+                      << static_cast<int>(bus.cpu.flag_register) << '\n';
+        }
+        ASSERT_EQ(compare_flag_values, false);
+    }
+
+}
+
+
+TEST_F(RotateTest, RL_indexed_ix_test){
+    std::vector<int> expected_flag_values = {0x00, 0x00, 0x01, 0x84, 0x84, 0x84, 0x05};
+    bool compare_flag_values = false;
+
+    std::vector<uint8_t> memory = {
+            0xDD, 0x21, 0x00, 0x01, 0xDD,
+            0x36, 0xFF, 0x76, 0x3F, 0xDD, 0xCB, 0xFF, 0x16,
+            0x00, 0xDD, 0x36, 0xFF, 0x8F, 0xDD, 0xCB, 0xFF, 0x16
+    };
+
+    for (int i = 0; i < memory.size(); i++) {
+        bus.rom_write(i,memory[i]);
+    }
+
+    for(uint16_t byte_counter = 0; byte_counter < expected_flag_values.size(); byte_counter++){
+        bus.cpu.instruction_cycle();
+
+        if(bus.cpu.flag_register != expected_flag_values[byte_counter]){
+            compare_flag_values = true;
+            std::cout << "OPCODE: " << std::setfill('0') << std::setw(2) << std::hex << std::uppercase
+                      << static_cast<int>(bus.cpu.opcode) << '\t';
+
+            std::cout << "Flag: 0x" << std::setfill('0') << std::setw(2) << std::hex << std::uppercase
+                      << static_cast<int>(bus.cpu.flag_register) << '\n';
+        }
+        ASSERT_EQ(compare_flag_values, false);
+    }
+}
+
+
+TEST_F(RotateTest, RL_indexed_iy_test){
+    std::vector<int> expected_flag_values = {0x00, 0x00, 0x01, 0x84, 0x84, 0x84, 0x05};
+    bool compare_flag_values = false;
+
+    std::vector<uint8_t> memory = {
+            0xFD, 0x21, 0x00, 0x01, 0xFD, 0x36, 0xFF, 0x76,
+            0x3F, 0xFD, 0xCB, 0xFF, 0x16, 0x00, 0xFD, 0x36,
+            0xFF, 0x8F, 0xFD, 0xCB, 0xFF, 0x16
+    };
+
+    for (int i = 0; i < memory.size(); i++) {
+        bus.rom_write(i,memory[i]);
+    }
+
+    for(uint16_t byte_counter = 0; byte_counter < expected_flag_values.size(); byte_counter++){
+        bus.cpu.instruction_cycle();
+
+        if(bus.cpu.flag_register != expected_flag_values[byte_counter]){
+            compare_flag_values = true;
+            std::cout << "OPCODE: " << std::setfill('0') << std::setw(2) << std::hex << std::uppercase
+                      << static_cast<int>(bus.cpu.opcode) << '\t';
+
+            std::cout << "Flag: 0x" << std::setfill('0') << std::setw(2) << std::hex << std::uppercase
+                      << static_cast<int>(bus.cpu.flag_register) << '\n';
+        }
+        ASSERT_EQ(compare_flag_values, false);
+    }
+}
+
+
