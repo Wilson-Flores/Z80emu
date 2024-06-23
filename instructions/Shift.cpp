a520c9b0474443a617cb3ef40d915558a9b86247
Wilson-Flores <WillyFlores173@gmail.com>
2024-06-22 22:54:22 -0400

    Added SRL Instructions.

--- instructions/Shift.cpp
+++ instructions/Shift.cpp
@@ -155,7 +155,7 @@ void z80cpu::SRA_implied() {
     set_flag(PARITY_OVERFLOW_FLAG, PARITY_TABLE[result]);
     // N is reset
     set_flag(ADD_SUB_FLAG, false);
-    // C is data from bit 7 of source register
+    // C is data from bit 0 of source register
 }
 
 
@@ -191,7 +191,7 @@ void z80cpu::SRA_indirect() {
     set_flag(PARITY_OVERFLOW_FLAG, PARITY_TABLE[result]);
     // N is reset
     set_flag(ADD_SUB_FLAG, false);
-    // C is data from bit 7 of source register
+    // C is data from bit 0 of source register
 }
 
 
@@ -226,7 +226,7 @@ void z80cpu::SRA_indexed_ix() {
     set_flag(PARITY_OVERFLOW_FLAG, PARITY_TABLE[result]);
     // N is reset
     set_flag(ADD_SUB_FLAG, false);
-    // C is data from bit 7 of source register
+    // C is data from bit 0 of source register
 }
 
 
@@ -253,6 +253,132 @@ void z80cpu::SRA_indexed_iy() {
 
     // S is set if result is negative, else reset
     set_flag(SIGN_FLAG, result & 0x80);
+    // Z is set if result is 0, else reset
+    set_flag(ZERO_FLAG, result == 0);
+    // H is reset
+    set_flag(HALF_CARRY_FLAG, false);
+    // P/V is set if parity even, else reset
+    set_flag(PARITY_OVERFLOW_FLAG, PARITY_TABLE[result]);
+    // N is reset
+    set_flag(ADD_SUB_FLAG, false);
+    // C is data from bit 0 of source register
+}
+
+
+void z80cpu::SRL_implied() {
+    t_state_cycles = 8;
+
+    // copy the data from the register
+    data = *register_table[opcode & BIT_MASK_2];
+
+    // clear the carry flag bit
+    flag_register &= 0xFE;
+    // copy data 0th bit into carry flag bit
+    flag_register |= (data & 0x01);
+
+    // We will be shifting bits to right
+    result = data >> 1;
+    *register_table[opcode & BIT_MASK_2] = result;
+
+    // S is reset
+    set_flag(SIGN_FLAG, false);
+    // Z is set if result is 0, else reset
+    set_flag(ZERO_FLAG, result == 0);
+    // H is reset
+    set_flag(HALF_CARRY_FLAG, false);
+    // P/V is set if parity even, else reset
+    set_flag(PARITY_OVERFLOW_FLAG, PARITY_TABLE[result]);
+    // N is reset
+    set_flag(ADD_SUB_FLAG, false);
+    // C is data from bit 0 of source register
+}
+
+
+void z80cpu::SRL_indirect() {
+    t_state_cycles = 15;
+
+    // copy the data from the memory address
+    address_absolute = (static_cast<uint16_t>(H_register) << 8) | L_register;
+    data = ram_read(address_absolute);
+
+    // clear the carry flag bit
+    flag_register &= 0xFE;
+    // copy data 0th bit into carry flag bit
+    flag_register |= (data & 0x01);
+
+    // We will be shifting bits to right
+    result = data >> 1;
+
+    // overwrite data
+    ram_write(address_absolute, result);
+
+
+    // S is reset
+    set_flag(SIGN_FLAG, false);
+    // Z is set if result is 0, else reset
+    set_flag(ZERO_FLAG, result == 0);
+    // H is reset
+    set_flag(HALF_CARRY_FLAG, false);
+    // P/V is set if parity even, else reset
+    set_flag(PARITY_OVERFLOW_FLAG, PARITY_TABLE[result]);
+    // N is reset
+    set_flag(ADD_SUB_FLAG, false);
+    // C is data from bit 7 of source register
+}
+
+
+void z80cpu::SRL_indexed_ix() {
+    t_state_cycles = 23;
+
+    // copy the data from the memory address
+    address_absolute = index_register_x + static_cast<int16_t>(displacement);
+    data = ram_read(address_absolute);
+
+    // clear the carry flag bit
+    flag_register &= 0xFE;
+    // copy data 0th bit into carry flag bit
+    flag_register |= (data & 0x01);
+
+    // We will be shifting bits to right
+    result = data >> 1;
+
+    // overwrite data
+    ram_write(address_absolute, result);
+
+    // S is reset
+    set_flag(SIGN_FLAG, false);
+    // Z is set if result is 0, else reset
+    set_flag(ZERO_FLAG, result == 0);
+    // H is reset
+    set_flag(HALF_CARRY_FLAG, false);
+    // P/V is set if parity even, else reset
+    set_flag(PARITY_OVERFLOW_FLAG, PARITY_TABLE[result]);
+    // N is reset
+    set_flag(ADD_SUB_FLAG, false);
+    // C is data from bit 7 of source register
+}
+
+
+void z80cpu::SRL_indexed_iy() {
+    t_state_cycles = 23;
+
+    // copy the data from the memory address
+    address_absolute = index_register_y + static_cast<int16_t>(displacement);
+    data = ram_read(address_absolute);
+
+    // clear the carry flag bit
+    flag_register &= 0xFE;
+    // copy data 0th bit into carry flag bit
+    flag_register |= (data & 0x01);
+
+    // We will be shifting bits to right
+    result = data >> 1;
+
+    // overwrite data
+    ram_write(address_absolute, result);
+
+    // S is reset
+    set_flag(SIGN_FLAG, false);
     // Z is set if result is 0, else reset
     set_flag(ZERO_FLAG, result == 0);
     // H is reset
--- z80.hpp
+++ z80.hpp
@@ -255,6 +255,12 @@ public:
     void SRA_indexed_ix();                                                   // [SRA (IX+d)]
     void SRA_indexed_iy();                                                   // [SRA (IY+d)]
 
+    // Shift Right Logical Instructions
+    void SRL_implied();                                                      // [SRL r]
+    void SRL_indirect();                                                     // [SRL (HL)]
+    void SRL_indexed_ix();                                                   // [SRL (IX+d)]
+    void SRL_indexed_iy();                                                   // [SRL (IY+d)]
+
     // NOP Instruction
     void NOP_implied();                                                     // [NOP]
 
