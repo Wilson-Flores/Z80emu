c668c48396678c33d556f68ea79e6269d4abab15
Wilson-Flores <WillyFlores173@gmail.com>
2024-07-28 20:19:57 -0400

    updated instructions to include WZ register.

--- instructions/Exchange.cpp
+++ instructions/Exchange.cpp
@@ -43,12 +43,16 @@ void z80cpu::EX_register_indirect_implied_hl() {
     t_state_cycles = 19;
 
     data_8 = ram_read(stack_pointer);  // low byte in HL is swapped with memory at stack pointer address
+    // WZ register stores the data from the stack pointer address starting with the low byte from l register
+    WZ_register = static_cast<uint16_t>(data_8);
     L_register ^= data_8;
     data_8 ^= L_register;
     L_register ^= data_8;
     ram_write(stack_pointer, data_8);
 
     data_8 = ram_read(stack_pointer + 1);     // high byte in HL is swapped with memory at (stack pointer + 1) address
+    // WZ register stores the data from the stack pointer address, the high byte is from H register.
+    WZ_register += static_cast<uint16_t>(data_8) << 8;
     H_register ^= data_8;
     data_8 ^= H_register;
     H_register ^= data_8;
@@ -60,6 +64,8 @@ void z80cpu::EX_register_indirect_implied_ix() {
     t_state_cycles = 19;
 
     data_16 = static_cast<uint16_t>(ram_read(stack_pointer + 1) << 8) | ram_read(stack_pointer);
+    // data is stored in wz_register
+    WZ_register = data_16;
     index_register_x ^= data_16;
     data_16 ^= index_register_x;
     index_register_x ^= data_16;
@@ -73,6 +79,8 @@ void z80cpu::EX_register_indirect_implied_iy() {
     t_state_cycles = 19;
 
     data_16 = static_cast<uint16_t>(ram_read(stack_pointer + 1) << 8) | ram_read(stack_pointer);
+    // data is stored in wz_register
+    WZ_register = data_16;
     index_register_y ^= data_16;
     data_16 ^= index_register_y;
     index_register_y ^= data_16;
