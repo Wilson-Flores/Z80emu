e75bf043a4db4e11d88fcd61d4301c6d84d8d7b7
Wilson-Flores <willyflores173@gmail.com>
2024-09-16 21:08:33 -0400

    Added [SET b, (HL)] instruction.

--- instructions/Set.cpp
+++ instructions/Set.cpp
@@ -5,7 +5,6 @@ void z80cpu::SET_implied() {
     t_state_cycles = 8;
 
     // data_8 will store what bit value we will be setting.
-    // register value will store the value at the register we want to work with.
     data_8 = (opcode & BIT_MASK_1) >> 3;
 
     // we will perform an OR operation with the desired bit value.
@@ -14,4 +13,22 @@ void z80cpu::SET_implied() {
     *register_table[opcode & BIT_MASK_2] |= BIT_MASKS[data_8];
 
     // No flag conditions affected
+}
+
+
+void z80cpu::SET_indirect() {
+    t_state_cycles = 15;
+
+    // data_8 will store what bit value we will be setting.
+    data_8 = (opcode & BIT_MASK_1) >> 3;
+
+    // get value at (HL) memory address
+    memory_address = (static_cast<uint16_t>(H_register) << 8) | L_register;
+    result_8 = ram_read(memory_address);
+
+    // we will perform an OR operation with the desired bit value.
+    result_8 |= BIT_MASKS[data_8];
+
+    // re-write the value at the memory address
+    ram_write(memory_address, result_8);
 }
