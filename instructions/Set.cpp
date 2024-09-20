aa5fdd72997d80e40d633f94cfd0fb499e3024c4
Wilson-Flores <willyflores173@gmail.com>
2024-09-20 19:59:53 -0400

    added test case for [SET b, (IX_d)] and SET b, (IY_d)] instruction.

--- instructions/Set.cpp
+++ instructions/Set.cpp
@@ -32,3 +32,45 @@ void z80cpu::SET_indirect() {
     // re-write the value at the memory address
     ram_write(memory_address, result_8);
 }
+
+
+void z80cpu::SET_indexed_ix() {
+    t_state_cycles = 23;
+
+    // data_8 will store what bit value we will be setting.
+    data_8 = (opcode & BIT_MASK_1) >> 3;
+
+    // copy the data from the memory address
+    memory_address = index_register_x + static_cast<int16_t>(displacement);
+    result_8 = ram_read(memory_address);
+
+    // WZ register is updated using memory address
+    WZ_register = memory_address;
+
+    // we will perform an OR operation with the desired bit value.
+    result_8 |= BIT_MASKS[data_8];
+
+    // re-write the value at the memory address
+    ram_write(memory_address, result_8);
+}
+
+
+void z80cpu::SET_indexed_iy() {
+    t_state_cycles = 23;
+
+    // data_8 will store what bit value we will be setting.
+    data_8 = (opcode & BIT_MASK_1) >> 3;
+
+    // copy the data from the memory address
+    memory_address = index_register_y + static_cast<int16_t>(displacement);
+    result_8 = ram_read(memory_address);
+
+    // WZ register is updated using memory address
+    WZ_register = memory_address;
+
+    // we will perform an OR operation with the desired bit value.
+    result_8 |= BIT_MASKS[data_8];
+
+    // re-write the value at the memory address
+    ram_write(memory_address, result_8);
+}
