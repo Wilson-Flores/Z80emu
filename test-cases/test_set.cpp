ffe89e7a2fea94e7dd03e817ad0c5857af1dcbec
Wilson-Flores <willyflores173@gmail.com>
2024-09-20 18:21:18 -0400

    added test case for [SET b, (HL)] instruction.

--- test-cases/test_set.cpp
+++ test-cases/test_set.cpp
@@ -11,6 +11,7 @@ protected:
 
 TEST_F(SetTest, SET_implied_test)
 {
+    // Set doesnt affect any flags
     std::vector<uint8_t> expected_flag_values(9,0);
     std::vector<uint8_t> expected_register_values = {0x01, 0x03, 0x07, 0x0F, 0x1F, 0x3F, 0x7F, 0xFF};
 
@@ -50,5 +51,40 @@ TEST_F(SetTest, SET_implied_test)
             }
             ASSERT_EQ(compare_flag_values, false);
         }
+    }
+}
+
+
+
+TEST_F(SetTest, SET_indirect_test)
+{
+    // Set doesnt affect any flags
+    std::vector<uint8_t> expected_flag_values(9,0);
+    std::vector<uint8_t> expected_register_values = {0x01, 0x03, 0x07, 0x0F, 0x1F, 0x3F, 0x7F, 0xFF};
+
+    bool compare_flag_values = false;
+
+    std::vector<uint8_t> memory = {
+        0x21, 0x50, 0x00, 0x36, 0x00, 0xCB, 0xC6, 0xCB,
+        0xCE, 0xCB, 0xD6, 0xCB, 0xDE, 0xCB, 0xE6, 0xCB,
+        0xEE, 0xCB, 0xF6, 0xCB, 0xFE
+    };
+
+    for (int j = 0; j < memory.size(); j++) {
+        bus.rom_write(j,memory[j]);
+    }
+
+    for(uint16_t byte_counter = 0; byte_counter < expected_flag_values.size(); byte_counter++){
+        bus.cpu.instruction_cycle();
+
+        if(bus.cpu.flag_register != expected_flag_values[byte_counter]){
+            compare_flag_values = true;
+            std::cout << "OPCODE: " << std::setfill('0') << std::setw(2) << std::hex << std::uppercase
+                      << static_cast<int>(bus.cpu.opcode) << '\t';
+
+            std::cout << "Flag: 0x" << std::setfill('0') << std::setw(2) << std::hex << std::uppercase
+                      << static_cast<int>(bus.cpu.flag_register) << '\n';
+        }
+        ASSERT_EQ(compare_flag_values, false);
     }
 }
