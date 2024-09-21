8f0453cfb1727c5f02482b06e897c51f7fc61ef0
Wilson-Flores <willyflores173@gmail.com>
2024-09-21 13:37:18 -0400

    added RES Instructions.

--- instructions/Reset.cpp
+++ instructions/Reset.cpp
@@ -1,13 +1,76 @@
 #include "../z80.hpp"
 
 
-void z80cpu::RES_implied() {}
+void z80cpu::RES_implied() {
+    t_state_cycles = 8;
+
+    // data_8 will store what bit value we will be resetting.
+    data_8 = (opcode & BIT_MASK_1) >> 3;
+
+    // we will perform an AND operation with the desired bit value.
+    // ex. RES 7, A; where A = 0x80
+    //  0b10000000 & 0b01111111 = 0b00000000
+    *register_table[opcode & BIT_MASK_2] &= RES_BIT_MASKS[data_8];
+
+    // No flag conditions affected
+}
 
 
-void z80cpu::RES_indirect() {}
+void z80cpu::RES_indirect() {
+    t_state_cycles = 15;
+
+    // data_8 will store what bit value we will be resetting.
+    data_8 = (opcode & BIT_MASK_1) >> 3;
+
+    // get value at (HL) memory address
+    memory_address = (static_cast<uint16_t>(H_register) << 8) | L_register;
+    result_8 = ram_read(memory_address);
+
+    // we will perform an AND operation with the desired bit value.
+    result_8 &= RES_BIT_MASKS[data_8];
+
+    // re-write the value at the memory address
+    ram_write(memory_address, result_8);
+}
 
 
-void z80cpu::RES_indexed_ix() {}
+void z80cpu::RES_indexed_ix() {
+    t_state_cycles = 23;
+
+    // data_8 will store what bit value we will be resetting.
+    data_8 = (opcode & BIT_MASK_1) >> 3;
+
+    // copy the data from the memory address
+    memory_address = index_register_x + static_cast<int16_t>(displacement);
+    result_8 = ram_read(memory_address);
+
+    // WZ register is updated using memory address
+    WZ_register = memory_address;
+
+    // we will perform an AND operation with the desired bit value.
+    result_8 &= RES_BIT_MASKS[data_8];
+
+    // re-write the value at the memory address
+    ram_write(memory_address, result_8);
+}
 
 
-void z80cpu::RES_indexed_iy() {}
+void z80cpu::RES_indexed_iy() {
+    t_state_cycles = 23;
+
+    // data_8 will store what bit value we will be resetting.
+    data_8 = (opcode & BIT_MASK_1) >> 3;
+
+    // copy the data from the memory address
+    memory_address = index_register_y + static_cast<int16_t>(displacement);
+    result_8 = ram_read(memory_address);
+
+    // WZ register is updated using memory address
+    WZ_register = memory_address;
+
+    // we will perform an AND operation with the desired bit value.
+    result_8 &= RES_BIT_MASKS[data_8];
+
+    // re-write the value at the memory address
+    ram_write(memory_address, result_8);
+}
