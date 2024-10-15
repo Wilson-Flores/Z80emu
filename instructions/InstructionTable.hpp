44a3ea61d385c064e86d721de9b0a091adf6b620
Wilson-Flores <willyflores173@gmail.com>
2024-10-15 17:15:36 -0400

    moved all the instruction includes into a single hpp file.

---
+++ instructions/InstructionTable.hpp
@@ -0,0 +1,25 @@
+#ifndef InstructionTable_hpp
+#define InstructionTable_hpp
+
+
+#include "Add.hpp"
+#include "And.hpp"
+#include "Bit.hpp"
+#include "Compare.hpp"
+#include "Dec.hpp"
+#include "Exchange.hpp"
+#include "Inc.hpp"
+#include "Jump.hpp"
+#include "Load.hpp"
+#include "Misc.hpp"
+#include "Or.hpp"
+#include "Pop.hpp"
+#include "Push.hpp"
+#include "Reset.hpp"
+#include "Rotate.hpp"
+#include "Set.hpp"
+#include "Shift.hpp"
+#include "Sub.hpp"
+#include "Xor.hpp"
+
+#endif
