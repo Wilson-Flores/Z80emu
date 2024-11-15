f1aa15ebf3fb88fbf3ab30b4caf7afa63681dd1e
Wilson-Flores <WillyFlores173@gmail.com>
2024-11-14 21:28:52 -0500

    created a test functions file to separate testregister and future functions for testing.

--- test-cases/test_main.hpp
+++ test-cases/test_main.hpp
@@ -12,31 +12,8 @@ protected:
     Bus bus;
 
     void TestRegister(const std::vector<uint8_t>& memory, const uint8_t& reg,
-        const std::vector<uint8_t>& expected_register_values) {
+        const std::vector<uint8_t>& expected_register_values);
 
-        bool compare_reg_values = false;
-
-        // Write memory to the bus
-        bus.rom_reset();
-        for (int j = 0; j < memory.size(); j++) {
-            bus.rom_write(j, memory[j]);
-        }
-
-        // Test register values
-        for (const uint8_t expected_reg_value : expected_register_values) {
-            bus.cpu_.instruction_cycle();
-            if (reg != expected_reg_value) {
-                compare_reg_values = true;
-                std::cout << "OPCODE: " << std::setfill('0') << std::setw(2) << std::hex << std::uppercase
-                          << static_cast<int>(bus.cpu_.get_opcode_address()) << '\t';
-                std::cout << "REG VALUE: 0x" << std::setfill('0') << std::setw(2) << std::hex << std::uppercase
-                          << static_cast<int>(reg) << '\n';
-                std::cout << "EXPECTED REG VALUE: 0x" << std::setfill('0') << std::setw(2) << std::hex << std::uppercase
-          << static_cast<int>(expected_reg_value) << '\n';
-            }
-            ASSERT_EQ(compare_reg_values, false);
-        }
-    }
 };
 
 // Use BusTest as the base class for all specific test cases
