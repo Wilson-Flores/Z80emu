652044ad5230bdc1531ecf8cfa0595a8589b45cc
Wilson-Flores <WillyFlores173@gmail.com>
2024-11-15 20:56:47 -0500

    created TestFlag function.

--- test-cases/test_functions.cpp
+++ test-cases/test_functions.cpp
@@ -11,17 +11,41 @@ void BusTest::TestRegister(const std::vector<uint8_t>& memory, const uint8_t& re
     }
 
     // Test register values
-    for (const uint8_t expected_reg_value : expected_register_values) {
+    for (const uint8_t reg_value : expected_register_values) {
         bus.cpu_.instruction_cycle();
-        if (reg != expected_reg_value) {
+        if (reg != reg_value) {
             compare_reg_values = true;
             std::cout << "OPCODE: " << std::setfill('0') << std::setw(2) << std::hex << std::uppercase
             << static_cast<int>(bus.cpu_.get_opcode_address()) << '\t';
             std::cout << "REG VALUE: 0x" << std::setfill('0') << std::setw(2) << std::hex << std::uppercase
             << static_cast<int>(reg) << '\n';
             std::cout << "EXPECTED REG VALUE: 0x" << std::setfill('0') << std::setw(2) << std::hex << std::uppercase
-            << static_cast<int>(expected_reg_value) << '\n';
+                      << static_cast<int>(reg_value) << '\n';
         }
         ASSERT_EQ(compare_reg_values, false);
+    }
+}
+
+
+void BusTest::TestFlag(const std::vector<uint8_t> &memory, const uint8_t &flag,
+                       const std::vector<uint8_t> &expected_flag_values) {
+    bool compare_flag_values = false;
+    bus.rom_reset();
+    for (int j = 0; j < memory.size(); j++) {
+        bus.rom_write(j, memory[j]);
+    }
+
+    for (const uint8_t flag_value : expected_flag_values) {
+        bus.cpu_.instruction_cycle();
+        if (flag != flag_value) {
+            compare_flag_values = true;
+            std::cout << "OPCODE: " << std::setfill('0') << std::setw(2) << std::hex << std::uppercase
+                      << static_cast<int>(bus.cpu_.get_opcode_address()) << '\t';
+            std::cout << "FLAG VALUE: 0x" << std::setfill('0') << std::setw(2) << std::hex << std::uppercase
+                      << static_cast<int>(flag) << '\n';
+            std::cout << "EXPECTED FLAG VALUE: 0x" << std::setfill('0') << std::setw(2) << std::hex << std::uppercase
+                      << static_cast<int>(flag_value) << '\n';
+        }
+        ASSERT_EQ(compare_flag_values, false);
     }
 }
