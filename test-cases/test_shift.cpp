90a56ce91556a97e6bdbe9018a7de510c930e15c
Wilson-Flores <WillyFlores173@gmail.com>
2024-11-15 22:49:54 -0500

    fixed SLA implied test

--- test-cases/test_shift.cpp
+++ test-cases/test_shift.cpp
@@ -1,47 +1,30 @@
-//#include "test_main.hpp"
-//
-//
-//TEST_F(ShiftTest, SLA_implied_test){
-//    // {first, second}
-//    // {[LD r, n], [RLC r]}
-//    std::vector<std::pair<uint8_t, uint8_t>> registers_instructions =
-//            {{0x3E, 0x27}, // A
-//             {0x06, 0x20}, // B
-//             {0x0E, 0x21}, // C
-//             {0x16, 0x22}, // D
-//             {0x1E, 0x23}, // E
-//             {0x26, 0x24}, // H
-//             {0x2E, 0x25}  // L
-//            };
-//
-//    std::vector<uint8_t> expected_flag_values = {0x00, 0x01, 0x80};
-//    bool compare_flag_values = false;
-//
-//    for (const std::pair<const uint8_t, uint8_t>& pair : registers_instructions) {
-//        std::vector<uint8_t> memory = {pair.first, 0xB1, 0xCB, pair.second, 0xCB, pair.second};
-//
-//        bus.rom_reset();
-//
-//        for (int i = 0; i < memory.size(); i++) {
-//            bus.rom_write(i,memory[i]);
-//        }
-//
-//        for(uint16_t byte_counter = 0; byte_counter < expected_flag_values.size(); byte_counter++){
-//            bus.cpu.instruction_cycle();
-//
-//            if(bus.cpu.get_flag_register_value() != expected_flag_values[byte_counter]){
-//                compare_flag_values = true;
-//                std::cout << "OPCODE: " << std::setfill('0') << std::setw(2) << std::hex << std::uppercase
-//                          << static_cast<int>(bus.cpu.opcode) << '\t';
-//
-//                std::cout << "Flag: 0x" << std::setfill('0') << std::setw(2) << std::hex << std::uppercase
-//                          << static_cast<int>(bus.cpu.get_flag_register_value()) << '\n';
-//            }
-//            ASSERT_EQ(compare_flag_values, false);
-//        }
-//    }
-//}
-//
+#include "test_main.hpp"
+
+
+TEST_F(ShiftTest, SLA_implied_test)
+{
+    std::vector<uint8_t> expected_flag_values = {0x00, 0x21, 0x80};
+
+    // Memory sets for each register
+    std::vector<uint8_t> memory_A = {0x3E, 0xB1, 0xCB, 0x27, 0xCB, 0x27};
+    std::vector<uint8_t> memory_B = {0x06, 0xB1, 0xCB, 0x20, 0xCB, 0x20};
+    std::vector<uint8_t> memory_C = {0x0E, 0xB1, 0xCB, 0x21, 0xCB, 0x21};
+    std::vector<uint8_t> memory_D = {0x16, 0xB1, 0xCB, 0x22, 0xCB, 0x22};
+    std::vector<uint8_t> memory_E = {0x1E, 0xB1, 0xCB, 0x23, 0xCB, 0x23};
+    std::vector<uint8_t> memory_H = {0x26, 0xB1, 0xCB, 0x24, 0xCB, 0x24};
+    std::vector<uint8_t> memory_L = {0x2E, 0xB1, 0xCB, 0x25, 0xCB, 0x25};
+
+    // Run tests for each register
+    TestFlag(memory_A, bus.cpu_.get_flag_register_address(), expected_flag_values);
+    TestFlag(memory_B, bus.cpu_.get_flag_register_address(), expected_flag_values);
+    TestFlag(memory_C, bus.cpu_.get_flag_register_address(), expected_flag_values);
+    TestFlag(memory_D, bus.cpu_.get_flag_register_address(), expected_flag_values);
+    TestFlag(memory_E, bus.cpu_.get_flag_register_address(), expected_flag_values);
+    TestFlag(memory_H, bus.cpu_.get_flag_register_address(), expected_flag_values);
+    TestFlag(memory_L, bus.cpu_.get_flag_register_address(), expected_flag_values);
+}
+
+
 //TEST_F(ShiftTest, SLA_indirect_test){
 //    std::vector<uint8_t> expected_flag_values = {0x00, 0x00, 0x01, 0x80};
 //    bool compare_flag_values = false;
