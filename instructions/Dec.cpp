199b62d33cb170029b29287a759e41ac5aabfa8d
Wilson-Flores <willyflores173@gmail.com>
2024-11-20 12:18:21 -0500

    Replaced magic numbers with constexpr values.

--- instructions/Dec.cpp
+++ instructions/Dec.cpp
@@ -10,22 +10,22 @@ void z80cpu::DEC_implied_register() {
     result_8_ = data_8_ - 1;
 
     // S is set if result is negative, else reset
-    set_flag(SIGN_FLAG, result_8_ & 0x80);
+    set_flag(SIGN_FLAG, result_8_ & SIGN_MASK);
     // Z is set if result is 0, else reset
     set_flag(ZERO_FLAG, result_8_ == 0);
     // H is set if carry from bit 3;
     // only time half carry occurs: (0b0000 - 0b0001)
-    set_flag(HALF_CARRY_FLAG, ((data_8_ & 0x0F) == 0));
+    set_flag(HALF_CARRY_FLAG, ((data_8_ & HALF_CARRY_THRESHOLD) == 0));
     // P/V is set if r was 80h before operation, else reset
-    set_flag(PARITY_OVERFLOW_FLAG, data_8_ == 0x80);
+    set_flag(PARITY_OVERFLOW_FLAG, data_8_ == PARITY_OVERFLOW_MASK);
     // N is set
     set_flag(ADD_SUB_FLAG, true);
 
     *register_table_[register_bit] = result_8_;
 
     //X & Y Flags are copies bit 3 & 5 of the register
-    set_flag(X_FLAG, result_8_ & 0x08);
-    set_flag(Y_FLAG, result_8_ & 0x20);
+    set_flag(X_FLAG, result_8_ & X_FLAG_MASK);
+    set_flag(Y_FLAG, result_8_ & Y_FLAG_MASK);
 }
 
 
@@ -37,20 +37,20 @@ void z80cpu::DEC_implied_register_indirect() {
     result_8_ = data_8_ - 1;
 
     // S is set if result is negative, else reset
-    set_flag(SIGN_FLAG, result_8_ & 0x80);
+    set_flag(SIGN_FLAG, result_8_ & SIGN_MASK);
     // Z is set if result is 0, else reset
     set_flag(ZERO_FLAG, result_8_ == 0);
     // H is set if carry from bit 3;
     // only time half carry occurs: (0b0000 - 0b0001)
-    set_flag(HALF_CARRY_FLAG, ((data_8_ & 0x0F) == 0));
+    set_flag(HALF_CARRY_FLAG, ((data_8_ & HALF_CARRY_THRESHOLD) == 0));
     // P/V is set if r was 80h before operation, else reset
-    set_flag(PARITY_OVERFLOW_FLAG, data_8_ == 0x80);
+    set_flag(PARITY_OVERFLOW_FLAG, data_8_ == PARITY_OVERFLOW_MASK);
     // N is set
     set_flag(ADD_SUB_FLAG, true);
 
     //X & Y Flags are copies bit 3 & 5 of the register
-    set_flag(X_FLAG, result_8_ & 0x08);
-    set_flag(Y_FLAG, result_8_ & 0x20);
+    set_flag(X_FLAG, result_8_ & X_FLAG_MASK);
+    set_flag(Y_FLAG, result_8_ & Y_FLAG_MASK);
 
     ram_write(memory_address_, result_8_);
 }
@@ -70,20 +70,20 @@ void z80cpu::DEC_implied_indexed_ix() {
     result_8_ = data_8_ - 1;
 
     // S is set if result is negative, else reset
-    set_flag(SIGN_FLAG, result_8_ & 0x80);
+    set_flag(SIGN_FLAG, result_8_ & SIGN_MASK);
     // Z is set if result is 0, else reset
     set_flag(ZERO_FLAG, result_8_ == 0);
     // H is set if carry from bit 3;
     // only time half carry occurs: (0b0000 - 0b0001)
-    set_flag(HALF_CARRY_FLAG, ((data_8_ & 0x0F) == 0));
+    set_flag(HALF_CARRY_FLAG, ((data_8_ & HALF_CARRY_THRESHOLD) == 0));
     // P/V is set if r was 80h before operation, else reset
-    set_flag(PARITY_OVERFLOW_FLAG, data_8_ == 0x80);
+    set_flag(PARITY_OVERFLOW_FLAG, data_8_ == PARITY_OVERFLOW_MASK);
     // N is set
     set_flag(ADD_SUB_FLAG, true);
 
     //X & Y Flags are copies bit 3 & 5 of the register
-    set_flag(X_FLAG, result_8_ & 0x08);
-    set_flag(Y_FLAG, result_8_ & 0x20);
+    set_flag(X_FLAG, result_8_ & X_FLAG_MASK);
+    set_flag(Y_FLAG, result_8_ & Y_FLAG_MASK);
 
     ram_write(memory_address_, result_8_);
 }
@@ -103,20 +103,20 @@ void z80cpu::DEC_implied_indexed_iy() {
     result_8_ = data_8_ - 1;
 
     // S is set if result is negative, else reset
-    set_flag(SIGN_FLAG, result_8_ & 0x80);
+    set_flag(SIGN_FLAG, result_8_ & SIGN_MASK);
     // Z is set if result is 0, else reset
     set_flag(ZERO_FLAG, result_8_ == 0);
     // H is set if carry from bit 3;
     // only time half carry occurs: (0b0000 - 0b0001)
-    set_flag(HALF_CARRY_FLAG, ((data_8_ & 0x0F) == 0));
+    set_flag(HALF_CARRY_FLAG, ((data_8_ & HALF_CARRY_THRESHOLD) == 0));
     // P/V is set if r was 80h before operation, else reset
-    set_flag(PARITY_OVERFLOW_FLAG, data_8_ == 0x80);
+    set_flag(PARITY_OVERFLOW_FLAG, data_8_ == PARITY_OVERFLOW_MASK);
     // N is set
     set_flag(ADD_SUB_FLAG, true);
 
     //X & Y Flags are copies bit 3 & 5 of the register
-    set_flag(X_FLAG, result_8_ & 0x08);
-    set_flag(Y_FLAG, result_8_ & 0x20);
+    set_flag(X_FLAG, result_8_ & X_FLAG_MASK);
+    set_flag(Y_FLAG, result_8_ & Y_FLAG_MASK);
 
     ram_write(memory_address_, result_8_);
 }
@@ -132,8 +132,8 @@ void z80cpu::DEC_implied_register_extended(){
     data_16_ = (high_byte << 8) + low_byte;
     data_8_--;
 
-    *register_pair_table_ss_[register_pair_bit].high_byte_register = static_cast<uint8_t>((data_8_ & 0xFF00) >> 8);
-    *register_pair_table_ss_[register_pair_bit].low_byte_register = static_cast<uint8_t>(data_8_ & 0x00FF);
+    *register_pair_table_ss_[register_pair_bit].high_byte_register = static_cast<uint8_t>((data_8_ & HIGH_BYTE_MASK) >> 8);
+    *register_pair_table_ss_[register_pair_bit].low_byte_register = static_cast<uint8_t>(data_8_ & LOW_BYTE_MASK_2);
 }
 
 
