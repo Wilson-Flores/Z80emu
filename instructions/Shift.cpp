beffb3bb473612a418fda6c15887d8f7c2f6fec4
Wilson-Flores <WillyFlores173@gmail.com>
2024-11-20 21:00:03 -0500

    changed magic numbers to constexpr values.

--- instructions/Shift.cpp
+++ instructions/Shift.cpp
@@ -8,7 +8,7 @@ void z80cpu::SLA_implied() {
     data_8_ = *register_table_[opcode_ & BIT_MASK_2];
 
     // clear the carry flag bit
-    flag_register_ &= 0xFE;
+    flag_register_ &= BIT_MASK_6;
     // copy data 7th bit into carry flag bit
     flag_register_ |= (data_8_ >> 7);
 
@@ -17,7 +17,7 @@ void z80cpu::SLA_implied() {
     *register_table_[opcode_ & BIT_MASK_2] = result_8_;
 
     // S is set if result is negative, else reset
-    set_flag(SIGN_FLAG, result_8_ & 0x80);
+    set_flag(SIGN_FLAG, result_8_ & BIT_MASK_9);
     // Z is set if result is 0, else reset
     set_flag(ZERO_FLAG, result_8_ == 0);
     // H is reset
@@ -29,8 +29,8 @@ void z80cpu::SLA_implied() {
     // C is data from bit 7 of source register
 
     //X & Y Flags are copies bit 3 & 5 of the register
-    set_flag(X_FLAG, result_8_ & 0x08);
-    set_flag(Y_FLAG, result_8_ & 0x20);
+    set_flag(X_FLAG, result_8_ & X_FLAG_MASK);
+    set_flag(Y_FLAG, result_8_ & Y_FLAG_MASK);
 }
 
 
@@ -42,7 +42,7 @@ void z80cpu::SLA_indirect() {
     data_8_ = ram_read(memory_address_);
 
     // clear the carry flag bit
-    flag_register_ &= 0xFE;
+    flag_register_ &= BIT_MASK_6;
     // copy data 7th bit into carry flag bit
     flag_register_ |= (data_8_ >> 7);
 
@@ -54,7 +54,7 @@ void z80cpu::SLA_indirect() {
 
 
     // S is set if result is negative, else reset
-    set_flag(SIGN_FLAG, result_8_ & 0x80);
+    set_flag(SIGN_FLAG, result_8_ & BIT_MASK_9);
     // Z is set if result is 0, else reset
     set_flag(ZERO_FLAG, result_8_ == 0);
     // H is reset
@@ -66,8 +66,8 @@ void z80cpu::SLA_indirect() {
     // C is data from bit 7 of source register
 
     //X & Y Flags are copies bit 3 & 5 of the register
-    set_flag(X_FLAG, result_8_ & 0x08);
-    set_flag(Y_FLAG, result_8_ & 0x20);
+    set_flag(X_FLAG, result_8_ & X_FLAG_MASK);
+    set_flag(Y_FLAG, result_8_ & Y_FLAG_MASK);
 }
 
 
@@ -82,7 +82,7 @@ void z80cpu::SLA_indexed_ix() {
     WZ_register_ = memory_address_;
 
     // clear the carry flag bit
-    flag_register_ &= 0xFE;
+    flag_register_ &= BIT_MASK_6;
     // copy data 7th bit into carry flag bit
     flag_register_ |= (data_8_ >> 7);
 
@@ -93,7 +93,7 @@ void z80cpu::SLA_indexed_ix() {
     ram_write(memory_address_, result_8_);
 
     // S is set if result is negative, else reset
-    set_flag(SIGN_FLAG, result_8_ & 0x80);
+    set_flag(SIGN_FLAG, result_8_ & BIT_MASK_8);
     // Z is set if result is 0, else reset
     set_flag(ZERO_FLAG, result_8_ == 0);
     // H is reset
@@ -105,8 +105,8 @@ void z80cpu::SLA_indexed_ix() {
     // C is data from bit 7 of source register
 
     //X & Y Flags are copies bit 3 & 5 of the register
-    set_flag(X_FLAG, result_8_ & 0x08);
-    set_flag(Y_FLAG, result_8_ & 0x20);
+    set_flag(X_FLAG, result_8_ & X_FLAG_MASK);
+    set_flag(Y_FLAG, result_8_ & Y_FLAG_MASK);
 }
 
 
@@ -121,7 +121,7 @@ void z80cpu::SLA_indexed_iy() {
     WZ_register_ = memory_address_;
 
     // clear the carry flag bit
-    flag_register_ &= 0xFE;
+    flag_register_ &= BIT_MASK_6;
     // copy data 7th bit into carry flag bit
     flag_register_ |= (data_8_ >> 7);
 
@@ -132,7 +132,7 @@ void z80cpu::SLA_indexed_iy() {
     ram_write(memory_address_, result_8_);
 
     // S is set if result is negative, else reset
-    set_flag(SIGN_FLAG, result_8_ & 0x80);
+    set_flag(SIGN_FLAG, result_8_ & BIT_MASK_9);
     // Z is set if result is 0, else reset
     set_flag(ZERO_FLAG, result_8_ == 0);
     // H is reset
@@ -144,8 +144,8 @@ void z80cpu::SLA_indexed_iy() {
     // C is data from bit 7 of source register
 
     //X & Y Flags are copies bit 3 & 5 of the register
-    set_flag(X_FLAG, result_8_ & 0x08);
-    set_flag(Y_FLAG, result_8_ & 0x20);
+    set_flag(X_FLAG, result_8_ & X_FLAG_MASK);
+    set_flag(Y_FLAG, result_8_ & Y_FLAG_MASK);
 }
 
 
@@ -156,19 +156,19 @@ void z80cpu::SRA_implied() {
     data_8_ = *register_table_[opcode_ & BIT_MASK_2];
 
     // clear the carry flag bit
-    flag_register_ &= 0xFE;
+    flag_register_ &= BIT_MASK_6;
     // copy data 0th bit into carry flag bit
-    flag_register_ |= (data_8_ & 0x01);
+    flag_register_ |= (data_8_ & BIT_MASK_5);
 
     // We will be shifting bits to right while keeping the 7 bit the same.
     // EXAMPLE:
     // 7 6 5 4 3 2 1 0 C     7 6 5 4 3 2 1 0 C
     // 1 0 1 1 1 0 0 0 0 ->  1 1 0 1 1 1 0 0 0
-    result_8_ = (data_8_ >> 1) | (data_8_ & 0x80);
+    result_8_ = (data_8_ >> 1) | (data_8_ & BIT_MASK_9);
     *register_table_[opcode_ & BIT_MASK_2] = result_8_;
 
     // S is set if result is negative, else reset
-    set_flag(SIGN_FLAG, result_8_ & 0x80);
+    set_flag(SIGN_FLAG, result_8_ & BIT_MASK_9);
     // Z is set if result is 0, else reset
     set_flag(ZERO_FLAG, result_8_ == 0);
     // H is reset
@@ -180,8 +180,8 @@ void z80cpu::SRA_implied() {
     // C is data from bit 0 of source register
 
     //X & Y Flags are copies bit 3 & 5 of the register
-    set_flag(X_FLAG, result_8_ & 0x08);
-    set_flag(Y_FLAG, result_8_ & 0x20);
+    set_flag(X_FLAG, result_8_ & X_FLAG_MASK);
+    set_flag(Y_FLAG, result_8_ & Y_FLAG_MASK);
 }
 
 
@@ -195,20 +195,20 @@ void z80cpu::SRA_indirect() {
     // clear the carry flag bit
     flag_register_ &= 0xFE;
     // copy data 0th bit into carry flag bit
-    flag_register_ |= (data_8_ & 0x01);
+    flag_register_ |= (data_8_ & BIT_MASK_5);
 
     // We will be shifting bits to right while keeping the 7 bit the same.
     // EXAMPLE:
     // 7 6 5 4 3 2 1 0 C     7 6 5 4 3 2 1 0 C
     // 1 0 1 1 1 0 0 0 0 ->  1 1 0 1 1 1 0 0 0
-    result_8_ = (data_8_ >> 1) | (data_8_ & 0x80);
+    result_8_ = (data_8_ >> 1) | (data_8_ & BIT_MASK_9);
 
     // overwrite data
     ram_write(memory_address_, result_8_);
 
 
     // S is set if result is negative, else reset
-    set_flag(SIGN_FLAG, result_8_ & 0x80);
+    set_flag(SIGN_FLAG, result_8_ & BIT_MASK_9);
     // Z is set if result is 0, else reset
     set_flag(ZERO_FLAG, result_8_ == 0);
     // H is reset
@@ -220,8 +220,8 @@ void z80cpu::SRA_indirect() {
     // C is data from bit 0 of source register
 
     //X & Y Flags are copies bit 3 & 5 of the register
-    set_flag(X_FLAG, result_8_ & 0x08);
-    set_flag(Y_FLAG, result_8_ & 0x20);
+    set_flag(X_FLAG, result_8_ & X_FLAG_MASK);
+    set_flag(Y_FLAG, result_8_ & Y_FLAG_MASK);
 }
 
 
@@ -236,21 +236,21 @@ void z80cpu::SRA_indexed_ix() {
     WZ_register_ = memory_address_;
 
     // clear the carry flag bit
-    flag_register_ &= 0xFE;
+    flag_register_ &= BIT_MASK_6;
     // copy data 0th bit into carry flag bit
-    flag_register_ |= (data_8_ & 0x01);
+    flag_register_ |= (data_8_ & BIT_MASK_5);
 
     // We will be shifting bits to right while keeping the 7 bit the same.
     // EXAMPLE:
     // 7 6 5 4 3 2 1 0 C     7 6 5 4 3 2 1 0 C
     // 1 0 1 1 1 0 0 0 0 ->  1 1 0 1 1 1 0 0 0
-    result_8_ = (data_8_ >> 1) | (data_8_ & 0x80);
+    result_8_ = (data_8_ >> 1) | (data_8_ & BIT_MASK_9);
 
     // overwrite data
     ram_write(memory_address_, result_8_);
 
     // S is set if result is negative, else reset
-    set_flag(SIGN_FLAG, result_8_ & 0x80);
+    set_flag(SIGN_FLAG, result_8_ & BIT_MASK_9);
     // Z is set if result is 0, else reset
     set_flag(ZERO_FLAG, result_8_ == 0);
     // H is reset
@@ -262,8 +262,8 @@ void z80cpu::SRA_indexed_ix() {
     // C is data from bit 0 of source register
 
     //X & Y Flags are copies bit 3 & 5 of the register
-    set_flag(X_FLAG, result_8_ & 0x08);
-    set_flag(Y_FLAG, result_8_ & 0x20);
+    set_flag(X_FLAG, result_8_ & X_FLAG_MASK);
+    set_flag(Y_FLAG, result_8_ & Y_FLAG_MASK);
 }
 
 
@@ -278,21 +278,21 @@ void z80cpu::SRA_indexed_iy() {
     WZ_register_ = memory_address_;
 
     // clear the carry flag bit
-    flag_register_ &= 0xFE;
+    flag_register_ &= BIT_MASK_6;
     // copy data 0th bit into carry flag bit
-    flag_register_ |= (data_8_ & 0x01);
+    flag_register_ |= (data_8_ & BIT_MASK_5);
 
     // We will be shifting bits to right while keeping the 7 bit the same.
     // EXAMPLE:
     // 7 6 5 4 3 2 1 0 C     7 6 5 4 3 2 1 0 C
     // 1 0 1 1 1 0 0 0 0 ->  1 1 0 1 1 1 0 0 0
-    result_8_ = (data_8_ >> 1) | (data_8_ & 0x80);
+    result_8_ = (data_8_ >> 1) | (data_8_ & BIT_MASK_9);
 
     // overwrite data
     ram_write(memory_address_, result_8_);
 
     // S is set if result is negative, else reset
-    set_flag(SIGN_FLAG, result_8_ & 0x80);
+    set_flag(SIGN_FLAG, result_8_ & BIT_MASK_9);
     // Z is set if result is 0, else reset
     set_flag(ZERO_FLAG, result_8_ == 0);
     // H is reset
@@ -304,8 +304,8 @@ void z80cpu::SRA_indexed_iy() {
     // C is data from bit 0 of source register
 
     //X & Y Flags are copies bit 3 & 5 of the register
-    set_flag(X_FLAG, result_8_ & 0x08);
-    set_flag(Y_FLAG, result_8_ & 0x20);
+    set_flag(X_FLAG, result_8_ & X_FLAG_MASK);
+    set_flag(Y_FLAG, result_8_ & Y_FLAG_MASK);
 }
 
 
@@ -316,9 +316,9 @@ void z80cpu::SRL_implied() {
     data_8_ = *register_table_[opcode_ & BIT_MASK_2];
 
     // clear the carry flag bit
-    flag_register_ &= 0xFE;
+    flag_register_ &= BIT_MASK_6;
     // copy data 0th bit into carry flag bit
-    flag_register_ |= (data_8_ & 0x01);
+    flag_register_ |= (data_8_ & BIT_MASK_5);
 
     // We will be shifting bits to right
     result_8_ = data_8_ >> 1;
@@ -337,8 +337,8 @@ void z80cpu::SRL_implied() {
     // C is data from bit 0 of source register
 
     //X & Y Flags are copies bit 3 & 5 of the register
-    set_flag(X_FLAG, result_8_ & 0x08);
-    set_flag(Y_FLAG, result_8_ & 0x20);
+    set_flag(X_FLAG, result_8_ & X_FLAG_MASK);
+    set_flag(Y_FLAG, result_8_ & Y_FLAG_MASK);
 }
 
 
@@ -352,7 +352,7 @@ void z80cpu::SRL_indirect() {
     // clear the carry flag bit
     flag_register_ &= 0xFE;
     // copy data 0th bit into carry flag bit
-    flag_register_ |= (data_8_ & 0x01);
+    flag_register_ |= (data_8_ & BIT_MASK_5);
 
     // We will be shifting bits to right
     result_8_ = data_8_ >> 1;
@@ -374,8 +374,8 @@ void z80cpu::SRL_indirect() {
     // C is data from bit 7 of source register
 
     //X & Y Flags are copies bit 3 & 5 of the register
-    set_flag(X_FLAG, result_8_ & 0x08);
-    set_flag(Y_FLAG, result_8_ & 0x20);
+    set_flag(X_FLAG, result_8_ & X_FLAG_MASK);
+    set_flag(Y_FLAG, result_8_ & Y_FLAG_MASK);
 }
 
 
@@ -390,9 +390,9 @@ void z80cpu::SRL_indexed_ix() {
     WZ_register_ = memory_address_;
 
     // clear the carry flag bit
-    flag_register_ &= 0xFE;
+    flag_register_ &= BIT_MASK_6;
     // copy data 0th bit into carry flag bit
-    flag_register_ |= (data_8_ & 0x01);
+    flag_register_ |= (data_8_ & BIT_MASK_5);
 
     // We will be shifting bits to right
     result_8_ = data_8_ >> 1;
@@ -413,8 +413,8 @@ void z80cpu::SRL_indexed_ix() {
     // C is data from bit 7 of source register
 
     //X & Y Flags are copies bit 3 & 5 of the register
-    set_flag(X_FLAG, result_8_ & 0x08);
-    set_flag(Y_FLAG, result_8_ & 0x20);
+    set_flag(X_FLAG, result_8_ & X_FLAG_MASK);
+    set_flag(Y_FLAG, result_8_ & Y_FLAG_MASK);
 }
 
 
@@ -429,9 +429,9 @@ void z80cpu::SRL_indexed_iy() {
     WZ_register_ = memory_address_;
 
     // clear the carry flag bit
-    flag_register_ &= 0xFE;
+    flag_register_ &= BIT_MASK_6;
     // copy data 0th bit into carry flag bit
-    flag_register_ |= (data_8_ & 0x01);
+    flag_register_ |= (data_8_ & BIT_MASK_5);
 
     // We will be shifting bits to right
     result_8_ = data_8_ >> 1;
@@ -452,6 +452,6 @@ void z80cpu::SRL_indexed_iy() {
     // C is data from bit 7 of source register
 
     //X & Y Flags are copies bit 3 & 5 of the register
-    set_flag(X_FLAG, result_8_ & 0x08);
-    set_flag(Y_FLAG, result_8_ & 0x20);
+    set_flag(X_FLAG, result_8_ & X_FLAG_MASK);
+    set_flag(Y_FLAG, result_8_ & Y_FLAG_MASK);
 }
